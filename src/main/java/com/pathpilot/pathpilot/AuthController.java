82ec733462ec6d1e4560117cf9bfe80ed15370ba
Cyrila7 <Cyril.Annoh@mail.citytech.cuny.edu>
2026-04-24 14:42:54 -0400

    added CORS to the authcontroller

--- src/main/java/com/pathpilot/pathpilot/AuthController.java
+++ src/main/java/com/pathpilot/pathpilot/AuthController.java
@@ -4,11 +4,17 @@ package com.pathpilot.pathpilot;
 import java.util.Optional;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = {
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "https://path-pilot-rho.vercel.app"
+})
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
